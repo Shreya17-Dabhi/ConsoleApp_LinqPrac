7beeb7b371b689843a12a856024b3935b0b7df81
Shreya <info@truesys.co.in>
2022-07-11 10:27:14 +0530

    some changes

--- ConsoleApp_LinqPrac/Program.cs
+++ ConsoleApp_LinqPrac/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//ggg,hjfgjuggggggggggggjkg
+
 namespace ConsoleApp_LinqPrac
 {
     class Program
